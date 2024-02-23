4f5ae0abf44b56ed2fe86639a1b212bdb48a9f08
Chaemin <kcm9561@naver.com>
2024-02-24 06:27:14 +0900

    제출현황 조회에 페이징 기능 추가

--- src/main/java/Winter_Project/Semteul_Battle/repository/SubmitRepository.java
+++ src/main/java/Winter_Project/Semteul_Battle/repository/SubmitRepository.java
@@ -1,6 +1,8 @@
 package Winter_Project.Semteul_Battle.repository;
 
 import Winter_Project.Semteul_Battle.domain.Submit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.List;
 @Repository
 public interface SubmitRepository extends JpaRepository<Submit, Long> {
     List<Submit> findByContest_Id(Long contestId);
+    Page<Submit> findByContestId(Long contestId, Pageable pageable);
 }
